5fa76a1e39dd0823995b575a76dafdf6f1509d45
albeate <blaadino@protonmail.com>
2020-12-15 18:55:44 +0100

    have started on BookSetGenerator
    specificly sampleFromSetOfISBNs and nextSetOfStockBooks

--- acertainbookstore-assignment3/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
+++ acertainbookstore-assignment3/src/com/acertainbookstore/client/workloads/BookSetGenerator.java
@@ -1,8 +1,15 @@
 package com.acertainbookstore.client.workloads;
 
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import com.acertainbookstore.business.Book;
+import com.acertainbookstore.business.BookStoreBook;
 import com.acertainbookstore.business.StockBook;
+import com.acertainbookstore.utils.BookStoreConstants;
+import com.acertainbookstore.utils.BookStoreException;
+
+import java.util.Random; // added by beatZ
 
 /**
  * Helper class to generate stockbooks and isbns modelled similar to Random
@@ -21,7 +28,16 @@ public class BookSetGenerator {
 	 * @return
 	 */
 	public Set<Integer> sampleFromSetOfISBNs(Set<Integer> isbns, int num) {
-		return null;
+		if (num == 0 || isbns.size() == 0) {
+			return null;
+		}
+		Random rand = new Random();
+		Set<Integer> uniqueISBN = new HashSet<>();
+		while (isbns.size() < num) {
+			uniqueISBN.add(rand.nextInt(isbns.size()));
+		}
+		//TODO this is just returning a set of random int
+		return uniqueISBN;
 	}
 
 	/**
@@ -31,7 +47,17 @@ public class BookSetGenerator {
 	 * @return
 	 */
 	public Set<StockBook> nextSetOfStockBooks(int num) {
-		return null;
+		if (num == 0) {
+			return null;
+		}
+		Random rand = new Random();
+		Set<Integer> genBook = new HashSet<>();
+		for (int i = 0; i < num; i++){
+			genBook.add(rand.nextInt(i));
+		}
+		// TODO :: this is just returning a set of size num of random int
+		return (Set<StockBook>) genBook.stream();
 	}
-
 }
+
+
